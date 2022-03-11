1af97dc8110eb7b32fcb6156a3c37d83317e713b
S-MJ-H <s.mujeebulhasan@gmail.com>
2022-03-11 22:08:27 +0500

    added JS Learn

---
+++ Landing_page/js_learn/script.js
@@ -0,0 +1,31 @@
+
+/*
+function favoriteAnimal(animal) {
+	console.log(animal + " is my favorite animal!")
+  }
+  
+  favoriteAnimal('Goat')
+
+  function multiply(num1,num2) {
+    console.log(num1 * num2);
+  }
+
+  a = 3;
+  b = 3;
+  multiply(a,b);
+
+  
+function capitalize(string) {
+  console.log(string.charAt(0).toLowerCase() + string.slice(1));
+  
+}
+
+capitalize('hello');
+
+const string = 'hello';
+const string2 = string.slice(string.length -1);
+console.log();
+*/
+
+
+document.write('hello');
